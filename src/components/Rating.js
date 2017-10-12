96e99220a4ed3ea8bb05d590ddae611235636f86
Caue Marcondes <c.marcondes@zoover.nl>
2017-10-12 14:52:46 +0200

    creating rating component

---
+++ src/components/Rating.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { View, Text, StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    width: 28,
+    height: 28,
+    backgroundColor: 'red',
+    borderRadius: 50,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  value: {
+    backgroundColor: 'transparent',
+  },
+  r_: { backgroundColor: 'transparent'},
+  r_1: { backgroundColor: '#4F4F47' },
+  r_2: { backgroundColor: '#6B6C6C' },
+  r_3: { backgroundColor: '#8A9294' },
+  r_4: { backgroundColor: '#6CAABE' },
+  r_5: { backgroundColor: '#2F91BD' },
+  r_6: { backgroundColor: '#03a9f4' },
+  r_7: { backgroundColor: '#51C9FF' },
+  r_8: { backgroundColor: '#FFC618' },
+  r_9: { backgroundColor: '#FFA81D' },
+  r_10: { backgroundColor: '#FF8B22' },
+});
+
+class Rating extends Component {
+  static propTypes = {
+    value: PropTypes.number,
+  };
+
+  render() {
+    return (
+      <View style={[styles.container, styles[`r_${this.props.value}`]]}>
+        <Text style={styles.value}>{this.props.value}</Text>
+      </View>
+    );
+  }
+}
+
+export default Rating;
