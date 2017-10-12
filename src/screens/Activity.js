00da5eb95438a621ec130310b1b779417966d9c9
Caue Marcondes <c.marcondes@zoover.nl>
2017-10-12 16:17:28 +0200

    Creating list of activities

--- src/screens/Activity.js
+++ src/screens/Activity.js
@@ -1,15 +1,90 @@
 import React from 'react';
 import {
   Text,
+  Section,
+  StyleSheet,
+  View,
+  Image
 } from 'react-native';
 import { StackNavigator } from 'react-navigation';
+import Accordion from 'react-native-collapsible/Accordion';
+
+import Rating from '../components/Rating';
+import activities from '../static/activities';
+import forecast1Day from '../static/forecast1day';
+
+const styles = StyleSheet.create({
+  today__container: {
+    justifyContent: 'center',
+  },
+  today: {
+    fontSize: 12,
+    paddingTop: 20,
+    paddingBottom: 20,
+    paddingRight: 11,
+    textAlign: 'right'
+  },
+  container: {
+    paddingLeft: 11,
+    paddingRight: 11,
+  },
+  borderBottom: {
+    borderBottomColor: '#D8D8D8',
+    borderBottomWidth: 1,
+  },
+  row: {
+    paddingTop: 30,
+    paddingBottom: 30,
+    flexDirection: 'row',
+  },
+  headerText: {
+    flex: 1,
+    paddingTop: 6,
+    fontSize: 16,
+  },
+  icon: {
+    marginRight: 13,
+  }
+});
 
 class Activity extends React.Component {
   static navigationOptions = {
     title: 'Activity',
   };
+
+  _renderHeader(activity, index, isActive) {
+    const forecast = forecast1Day[0];
+    return (
+      <View style={[styles.container]}>
+        <View style={[ styles.row, (!isActive && styles.borderBottom) ]}>
+          <Text style={styles.headerText}>{activity.name}</Text>
+          <Image source={activity.icon} style={styles.icon}></Image>
+          <Rating value={forecast[`activity_digit_${activity.id}`]} />
+        </View>
+      </View>
+    );
+  }
+
+  _renderContent(activity, index, isActive) {
+    return (
+      <View style={styles.borderBottom}>
+        <Text style={styles.borderBottom}>CONTENT for {activity.name}</Text>
+      </View>
+    );
+  }
   render() {
-    return <Text>Hello, Activity!</Text>;
+    return (
+      <View>
+        <View style={[ styles.today__container, styles.borderBottom ]}>
+          <Text style={styles.today}>Vandaag</Text>
+        </View>
+        <Accordion
+          sections={activities}
+          renderHeader={this._renderHeader}
+          renderContent={this._renderContent}
+        />
+      </View>
+    );
   }
 }
 
---
+++ src/static/activities.js
@@ -0,0 +1,15 @@
+const activities = [
+  { id: 'bbq', name: 'Barbacue', icon: require('../assets/images/bbq.png')},
+  { id: 'cycling', name: 'Fietsen', icon: require('../assets/images/cycling.png')},
+  { id: 'golf', name: 'Golf', icon: require('../assets/images/golf.png')},
+  { id: 'camping', name: 'Camper', icon: require('../assets/images/camping.png')},
+  { id: 'short_skirts', name: 'Rokjes', icon: require('../assets/images/short_skirts.png')},
+  { id: 'beach', name: 'Strand', icon: require('../assets/images/beach.png')},
+  { id: 'tennis', name: 'Tennis', icon: require('../assets/images/tennis.png')},
+  { id: 'terrace', name: 'Terras', icon: require('../assets/images/terrace.png')},
+  { id: 'soccer', name: 'Voetbal', icon: require('../assets/images/soccer.png')},
+  { id: 'walking', name: 'Wandelen', icon: require('../assets/images/walking.png')},
+  { id: 'boat', name: 'Watersport', icon: require('../assets/images/boat.png')},
+]
+
+export default activities;
